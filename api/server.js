cebb4ca433d81e029fa66633cd8f26255703dc57
Yarriba Castro <yarribacastro@Yarribas-MacBook-Pro.local>
2020-05-27 00:09:48 -0500

    routes

--- api/server.js
+++ api/server.js
@@ -1,9 +1,24 @@
 const express = require("express");
 
-const db = require("../data/dbConfig.js");
+// const db = require("../data/dbConfig.js");
+
+// invoke Router files 
+const RouterData = require('./router-data/data-route.js');
 
 const server = express();
 
+// express middleware
 server.use(express.json());
+
+//Router handler
+server.use('/api/accounts', RouterData);
+
+server.get("/", (req, res) => {
+    res.status(200).json({
+        api: "it si running!"
+    });
+});
+
+
 
 module.exports = server;
---
+++ router-data/data-route.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+//database access using knex
+const knex = require('./data/dbConfig.js.js');
+// Invoke Router
+const router = express.Router();
+
+server.use('/', (req, res) => )
+
+//Database handlers and CRUD operations-------------
+
+
+//Export Router 
+module.exports = router;
